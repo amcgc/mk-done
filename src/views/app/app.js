991bc34737b527c86babf5b5aef71bcbde513149
amcgc <alex.cheers@gmail.com>
2018-03-17 11:50:50 +0000

    first change commit

--- src/views/app/app.js
+++ src/views/app/app.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
-import { authActions, getAuth } from 'src/auth';
-import Header from '../components/header';
-import RequireAuthRoute from '../components/require-auth-route';
-import RequireUnauthRoute from '../components/require-unauth-route';
-import SignInPage from '../pages/sign-in';
-import TasksPage from '../pages/tasks';
+import { authActions, getAuth } from "src/auth";
+import Header from "../components/header";
+import RequireAuthRoute from "../components/require-auth-route";
+import RequireUnauthRoute from "../components/require-unauth-route";
+import SignInPage from "../pages/sign-in";
+import TasksPage from "../pages/tasks";
 
-
-const App = ({authenticated, signOut}) => (
+const App = ({ authenticated, signOut }) => (
   <div>
-    <Header
-      authenticated={authenticated}
-      signOut={signOut}
-    />
+    <Header authenticated={authenticated} signOut={signOut} />
 
     <main>
-      <RequireAuthRoute authenticated={authenticated} exact path="/" component={TasksPage}/>
-      <RequireUnauthRoute authenticated={authenticated} path="/sign-in" component={SignInPage}/>
+      <RequireAuthRoute
+        authenticated={authenticated}
+        exact
+        path="/"
+        component={TasksPage}
+      />
+      <RequireUnauthRoute
+        authenticated={authenticated}
+        path="/sign-in"
+        component={SignInPage}
+      />
     </main>
   </div>
 );
@@ -30,10 +35,9 @@ App.propTypes = {
   signOut: PropTypes.func.isRequired
 };
 
-
-//=====================================
+//======================================
 //  CONNECT
-//-------------------------------------
+//--------------------------------------
 
 const mapStateToProps = getAuth;
 
@@ -41,9 +45,4 @@ const mapDispatchToProps = {
   signOut: authActions.signOut
 };
 
-export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(App)
-);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
