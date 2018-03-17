15cd54ca575416f1b6bd9fe81be538fc426a6367
amcgc <alex.cheers@gmail.com>
2018-03-17 13:51:54 +0000

    Remove unneeded github logo

--- src/views/components/header/header.js
+++ src/views/components/header/header.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Button from '../button';
-import GitHubLogo from '../github-logo';
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "../button";
 
-import './header.css';
+import "./header.css";
 
-
-const Header = ({authenticated, signOut}) => (
+const Header = ({ authenticated, signOut }) => (
   <header className="header">
     <div className="g-row">
       <div className="g-col">
-        <h1 className="header__title">Todo React Redux</h1>
+        <h1 className="header__title">Done</h1>
 
         <ul className="header__actions">
-          {authenticated ? <li><Button onClick={signOut}>Sign out</Button></li> : null}
-          <li>
-            <a className="link link--github" href="https://github.com/r-park/todo-react-redux">
-              <GitHubLogo />
-            </a>
-          </li>
+          {authenticated ? (
+            <li>
+              <Button onClick={signOut}>Sign out</Button>
+            </li>
+          ) : null}
         </ul>
       </div>
     </div>
@@ -29,6 +26,5 @@ Header.propTypes = {
   authenticated: PropTypes.bool.isRequired,
   signOut: PropTypes.func.isRequired
 };
-
 
 export default Header;
